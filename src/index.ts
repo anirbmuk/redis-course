b9fa7271dfd44a129a187b429da90d3fa9be09fa
anirbmuk <anirbmuk@live.in>
2023-03-26 13:18:42 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import express, { json } from 'express';
+import { createClient } from 'redis';
+import axios, { AxiosError, isAxiosError } from 'axios';
+
+const PREFIX = 'REDIS_TUTORIAL';
+const MOCK_SERVER = 'https://jsonplaceholder.typicode.com';
+
+type Post = {
+  userId: string;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const redisClient = createClient({
+  url: 'redis://127.0.0.1:6379'
+});
+
+const app = express();
+app.use(json());
+
+app.post('/', async (req, res) => {
+  const { key, value } = req.body;
+  const response = await redisClient.set(`${PREFIX}_${key}`, value);
+  res.status(204).json(response);
+});
+
+app.get<{ key: string }>('/:key', async (req, res) => {
+  const key = req.params.key;
+  const response = await redisClient.get(`${PREFIX}_${key}`);
+  res.status(200).json(response);
+});
+
+app.get<{ id: string }>('/posts/:id', async (req, res) => {
+  const key = req.params.id;
+  const cachedPost = (await redisClient.get(`${PREFIX}_POSTS_${key}`)) as
+    | string
+    | null;
+
+  if (cachedPost) {
+    // eslint-disable-next-line no-console
+    console.log('Returning data from cache');
+    return res.status(200).json(JSON.parse(cachedPost));
+  }
+
+  // eslint-disable-next-line no-console
+  console.warn(`No data found in cache for id: ${key}, fetching from server`);
+  let data = {} as Post;
+  try {
+    const response = await axios.get<Post>(`${MOCK_SERVER}/posts/${key}`);
+    data = response.data;
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      console.error(`${(error as AxiosError).code} - ${(error as AxiosError).message}`);
+    }
+  } finally {
+    await redisClient.set(`${PREFIX}_POSTS_${key}`, JSON.stringify(data), {
+      EX: 10
+    });
+  }
+  return res.status(200).json(data);
+});
+
+app.listen(3000, async () => {
+  await redisClient.connect();
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000');
+});
